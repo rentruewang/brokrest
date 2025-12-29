7b1cf1fab8ccf7927d28f59b46b92a921f480f5b
RenChu Wang <patrick1031wang@gmail.com>
2025-12-28 19:43:15 -0800

    🍳 Basic equations, candle charts, and plotting (#4)

--- src/brokrest/equations.py
+++ src/brokrest/equations.py
@@ -6,11 +6,6 @@ import abc
 import dataclasses as dcls
 import typing
 from abc import ABC
-from collections.abc import Sequence
-from typing import TypeAlias, TypeIs
-
-import torch
-from torch import Tensor
 
 from brokrest.vectors import Vec2d
 
@@ -18,29 +13,28 @@ from .painters import Canvas
 
 __all__ = ["LinearEq", "StandardForm", "SlopeInterceptForm", "InterceptForm"]
 
-_TensorLike: TypeAlias = int | float | Tensor
-
 
 class LinearEq(ABC):
     """
     A linear equation.
     """
 
-    def solve(self, x: _TensorLike, /) -> Tensor:
+    @abc.abstractmethod
+    def solve(self, x: float) -> float:
         """
         Get the y value of the line equation when x is given.
 
         Args:
-            x: The x value (can be a tensor).
+            x: The x value.
 
         Returns:
-            The y value (a tensor).
+            The y value.
         """
 
-        x = _promote_to_tensor(x)
-        return self._solve(x)
+        ...
 
-    def subs(self, x: _TensorLike, y: _TensorLike) -> Tensor:
+    @abc.abstractmethod
+    def subs(self, x: float, y: float) -> float:
         """
         The linear equation assumes that RHS = 0.
         This method applies x, y to the LHS, and get the value of RHS.
@@ -48,34 +42,8 @@ class LinearEq(ABC):
         If this method returns 0, the point is on the line.
         If 2 points (a, b), (c, d) have the same signs (both positive or both negative)
         after substituted into the equation, they are on the same side of the line.
-
-        Args:
-            x: The x values (can be a tensor, list, or scalar).
-            y: The y values (can be a tensor, list, or scalar).
-
-        Returns:
-            The result tensor.
         """
 
-        x = _promote_to_tensor(x)
-        y = _promote_to_tensor(y)
-
-        # To make the implementation easier below.
-        x, y = torch.broadcast_tensors(x, y)
-        assert x.ndim in [0, 1], f"Should have been a 1D or 2D tensor. Got {x.ndim}D."
-        assert x.shape == y.shape
-
-        return self._subs(x, y)
-
-    @abc.abstractmethod
-    def _solve(self, x: Tensor) -> Tensor:
-        "The implementation for ``.solve``."
-
-        ...
-
-    @abc.abstractmethod
-    def _subs(self, x: Tensor, y: Tensor) -> Tensor:
-        "The implementation for ``.sub``."
         ...
 
     def plot(self, canvas: Canvas) -> None:
@@ -85,7 +53,8 @@ class LinearEq(ABC):
         start_x = xs[0]
         end_x = xs[-1]
 
-        start_y, end_y = self.solve([start_x, end_x])
+        start_y = self.solve(start_x)
+        end_y = self.solve(end_x)
 
         canvas.line(Vec2d(start_x, start_y), Vec2d(end_x, end_y), color="blue")
 
@@ -112,12 +81,12 @@ class StandardForm(LinearEq):
     """
 
     @typing.override
-    def _solve(self, x: Tensor) -> Tensor:
+    def solve(self, x: float) -> float:
         # y = (ax + c) / -b
         return (self.a * x + self.c) / -self.b
 
     @typing.override
-    def _subs(self, x: Tensor, y: Tensor) -> Tensor:
+    def subs(self, x: float, y: float) -> float:
         return self.a * x + self.b * y + self.c
 
 
@@ -138,11 +107,11 @@ class SlopeInterceptForm(LinearEq):
     """
 
     @typing.override
-    def _solve(self, x: Tensor) -> Tensor:
+    def solve(self, x: float) -> float:
         return self.m * x + self.b
 
     @typing.override
-    def _subs(self, x: Tensor, y: Tensor) -> Tensor:
+    def subs(self, x: float, y: float) -> float:
         return self.m * x + self.b - y
 
 
@@ -165,33 +134,10 @@ class InterceptForm(LinearEq):
     """
 
     @typing.override
-    def _solve(self, x: Tensor) -> Tensor:
+    def solve(self, x: float) -> float:
         # y = -b (x/a - 1)
         return self.b - x * self.b / self.a
 
     @typing.override
-    def _subs(self, x: Tensor, y: Tensor) -> Tensor:
+    def subs(self, x: float, y: float) -> float:
         return x / self.a + y / self.b - 1
-
-
-def _promote_to_tensor(x: int | float | Sequence[int | float] | Tensor, /) -> Tensor:
-    def convert() -> Tensor:
-        if isinstance(x, Tensor):
-            return x
-
-        if isinstance(x, int | float):
-            return torch.tensor(x)
-
-        # Check this last because it can be expensive.
-        if _is_seq_of_numbers(x):
-            return torch.tensor(x)
-
-        raise TypeError(f"Unsupported type: {type(x)=}")
-
-    ans = convert()
-    assert ans.ndim in [0, 1], f"Should be a 0D or 1D Tensor. Got {ans.ndim}D"
-    return ans
-
-
-def _is_seq_of_numbers(x: object) -> TypeIs[Sequence[int | float]]:
-    return isinstance(x, Sequence) and all(isinstance(v, int | float) for v in x)

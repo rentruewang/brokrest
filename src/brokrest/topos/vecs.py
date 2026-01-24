df1188f9fb16b84225e17bd5ad909faecb95571a
RenChu Wang <patrick1031wang@gmail.com>
2026-01-24 11:02:46 -0800

    update vector

--- src/brokrest/topos/vecs.py
+++ src/brokrest/topos/vecs.py
@@ -6,7 +6,9 @@ import typing
 from abc import ABC
 from typing import ClassVar
 
+import torch
 from bokeh.plotting import figure as Figure
+from torch import Tensor
 
 from .rects import Box
 from .topos import Shape, Topo
@@ -17,6 +19,21 @@ __all__ = ["Vector", "Point"]
 class Vector(Topo, ABC):
     KEYS: ClassVar[tuple[str, ...]] = "x", "y"
 
+    def __matmul__(self, other: "Vector") -> Tensor:
+        """
+        Creates a m x n matrix O if len(self) = m and len(other) = n,
+        where O[p, q] = self[p] * other[q].
+
+        Args:
+            other: Another vector.
+
+        Returns:
+            A 2D matrix.
+        """
+
+        outer = lambda l, r, /: torch.einsum("m,n->mn", l, r)
+        return outer(self.x, other.x) + outer(self.y, other.y)
+
     @property
     def x(self):
         return self["x"]
@@ -24,6 +41,13 @@ class Vector(Topo, ABC):
     @property
     def y(self):
         return self["y"]
+
+    def unit(self):
+        return Vector(data=self.data / self.length)
+
+    @property
+    def length(self) -> Tensor:
+        return (self.x**2 + self.y**2) ** 0.5
 
 
 class Point(Vector, Shape):

0aa03a8dfd10c56c4dd8d32447b7d091a81107d4
RenChu Wang <patrick1031wang@gmail.com>
2026-02-26 00:37:36 -0800

    vecs are ok

--- src/brokrest/topos/vecs.py
+++ src/brokrest/topos/vecs.py
@@ -7,11 +7,13 @@ import typing
 from abc import ABC
 from collections.abc import Callable
 from numbers import Number
-from typing import ClassVar, Self, TypeAlias
+from typing import Any, Self
 
 import torch
 from bokeh.plotting import figure as Figure
 from torch import Tensor
+
+from brokrest import tds
 
 from .rects import Box
 from .topos import Shape, Topo
@@ -19,10 +21,17 @@ from .topos import Shape, Topo
 __all__ = ["Vector", "Point"]
 
 
-class Vector(Topo, ABC):
-    KEYS: ClassVar[tuple[str, ...]] = "x", "y"
+type ElemWiseRhs = "Number | Tensor | Vector"
 
-    ElemWiseRhs: TypeAlias = "Number | Vector"
+
+@tds.tensorclass
+class Vector(Topo, ABC):
+
+    x: Tensor
+    "The x element."
+
+    y: Tensor
+    "The y element."
 
     def __add__(self, other: ElemWiseRhs) -> Self:
         return _element_wise(self, other, operator.add)
@@ -75,49 +84,43 @@ class Vector(Topo, ABC):
         outer = lambda l, r, /: torch.einsum("m,n->mn", l, r)
         return outer(self.x, other.x) + outer(self.y, other.y)
 
-    @property
-    def x(self):
-        return self["x"]
-
-    @property
-    def y(self):
-        return self["y"]
-
-    def unit(self):
-        return Vector(data=self.data / self.length)
+    def unit(self) -> Self:
+        return self / self.length
 
     @property
     def length(self) -> Tensor:
         return (self.x**2 + self.y**2) ** 0.5
 
 
-def _element_wise(lhs, rhs, op: Callable[..., Tensor]):
-    match lhs, rhs:
-        # Even though both are ``Vector``,
-        # this would be called as a method on ``lhs``,
-        # so the return type is based on ``lhs``.
-        case Vector(), Vector():
-            return type(lhs).init(x=op(lhs.x, rhs.x), y=op(lhs.y, rhs.y))
+def _element_wise(lhs: Any, rhs: Any, op: Callable[..., Tensor]):
+    # Even though both are ``Vector``,
+    # this would be called as a method on ``lhs``,
+    # so the return type is based on ``lhs``.
+    if isinstance(lhs, Vector) and isinstance(rhs, Vector):
 
-        # For ``__op__``.
-        case Vector(), Number():
-            return type(lhs).init(x=op(lhs.x, rhs), y=op(lhs.y, rhs))
+        return type(lhs)(x=op(lhs.x, rhs.x), y=op(lhs.y, rhs.y))
 
-        # For ``__rop__``.
-        case Number(), Vector():
-            return type(rhs).init(x=op(lhs, rhs.x), y=op(lhs, rhs.y))
+    # For ``__op__``.
+    elif isinstance(lhs, Vector):
 
-        # This will never happen (if called as metohd), so it shall raise and error.
-        case _, _:
-            raise TypeError(f"Unrecognized types {type(lhs)=}, {type(rhs)=}.")
+        return type(lhs)(x=op(lhs.x, rhs), y=op(lhs.y, rhs))
+
+    # For ``__rop__``.
+    elif isinstance(rhs, Vector):
+        return type(rhs)(x=op(lhs, rhs.x), y=op(lhs, rhs.y))
+
+    # This will never happen (if called as metohd), so it shall raise and error.
+    else:
+        raise TypeError(f"Unrecognized types {type(lhs)=}, {type(rhs)=}.")
 
 
+@tds.tensorclass
 class Point(Vector, Shape):
     "A collection of points."
 
     @typing.override
     def _outer(self) -> Box:
-        return Box.init(x_0=self.x, x_1=self.x, y_0=self.y, y_1=self.y)
+        return Box(x_0=self.x, x_1=self.x, y_0=self.y, y_1=self.y)
 
     @typing.override
     def _draw(self, figure: Figure, /) -> None:

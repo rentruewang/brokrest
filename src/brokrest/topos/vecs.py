d4be51aec529fc74a4788980999f5516b7a91257
RenChu Wang <patrick1031wang@gmail.com>
2026-03-30 23:19:03 -0700

    2 more

--- src/brokrest/topos/vecs.py
+++ src/brokrest/topos/vecs.py
@@ -2,74 +2,71 @@
 
 "A set of points."
 
+import abc
+import numbers
 import operator
 import typing
-from abc import ABC
-from collections.abc import Callable
-from numbers import Number
-from typing import Any, Self
+from collections import abc as cabc
 
 import torch
-from bokeh.plotting import figure as Figure
-from torch import Tensor
+from bokeh import plotting
 
 from brokrest import tds
 
-from .rects import Box
-from .topos import Shape, Topo
+from . import rects, topos
 
 __all__ = ["Vector", "Point"]
 
 
-type ElemWiseRhs = "Number | Tensor | Vector"
+type ElemWiseRhs = "numbers.Number | torch.Tensor | Vector"
 
 
 @tds.tensorclass
-class Vector(Topo, ABC):
+class Vector(topos.Topo, abc.ABC):
 
-    x: Tensor
+    x: torch.Tensor
     "The x element."
 
-    y: Tensor
+    y: torch.Tensor
     "The y element."
 
-    def __add__(self, other: ElemWiseRhs) -> Self:
+    def __add__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(self, other, operator.add)
 
-    def __radd__(self, other: ElemWiseRhs) -> Self:
+    def __radd__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(other, self, operator.add)
 
-    def __sub__(self, other: ElemWiseRhs) -> Self:
+    def __sub__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(self, other, operator.sub)
 
-    def __rsub__(self, other: ElemWiseRhs) -> Self:
+    def __rsub__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(other, self, operator.sub)
 
-    def __mul__(self, other: ElemWiseRhs) -> Self:
+    def __mul__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(self, other, operator.mul)
 
-    def __rmul__(self, other: ElemWiseRhs) -> Self:
+    def __rmul__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(other, self, operator.mul)
 
-    def __truediv__(self, other: ElemWiseRhs) -> Self:
+    def __truediv__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(self, other, operator.truediv)
 
-    def __rtruediv__(self, other: ElemWiseRhs) -> Self:
+    def __rtruediv__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(other, self, operator.truediv)
 
-    def __floordiv__(self, other: ElemWiseRhs) -> Self:
+    def __floordiv__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(self, other, operator.floordiv)
 
-    def __rfloordiv__(self, other: ElemWiseRhs) -> Self:
+    def __rfloordiv__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(other, self, operator.floordiv)
 
-    def __pow__(self, other: ElemWiseRhs) -> Self:
+    def __pow__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(self, other, operator.pow)
 
-    def __rpow__(self, other: ElemWiseRhs) -> Self:
+    def __rpow__(self, other: ElemWiseRhs) -> typing.Self:
         return _element_wise(other, self, operator.pow)
 
-    def __matmul__(self, other: "Vector") -> Tensor:
+    def __matmul__(self, other: "Vector") -> torch.Tensor:
         """
         Creates a m x n matrix O if len(self) = m and len(other) = n,
         where O[p, q] = self[p] * other[q].
@@ -84,15 +81,17 @@ class Vector(Topo, ABC):
         outer = lambda l, r, /: torch.einsum("m,n->mn", l, r)
         return outer(self.x, other.x) + outer(self.y, other.y)
 
-    def unit(self) -> Self:
+    def unit(self) -> typing.Self:
         return self / self.length
 
     @property
-    def length(self) -> Tensor:
+    def length(self) -> torch.Tensor:
         return (self.x**2 + self.y**2) ** 0.5
 
 
-def _element_wise(lhs: Any, rhs: Any, op: Callable[..., Tensor]):
+def _element_wise(
+    lhs: typing.Any, rhs: typing.Any, op: cabc.Callable[..., torch.Tensor]
+):
     # Even though both are `Vector`,
     # this would be called as a method on `lhs`,
     # so the return type is based on `lhs`.
@@ -115,15 +114,15 @@ def _element_wise(lhs: Any, rhs: Any, op: Callable[..., Tensor]):
 
 
 @tds.tensorclass
-class Point(Vector, Shape):
+class Point(Vector, topos.Shape):
     "A collection of points."
 
     @typing.override
-    def _outer(self) -> Box:
-        return Box(x_0=self.x, x_1=self.x, y_0=self.y, y_1=self.y)
+    def _outer(self) -> rects.Box:
+        return rects.Box(x_0=self.x, x_1=self.x, y_0=self.y, y_1=self.y)
 
     @typing.override
-    def _draw(self, figure: Figure, /) -> None:
+    def _draw(self, figure: plotting.figure, /) -> None:
         _ = figure.scatter(
             x=self.x.numpy(),
             y=self.y.numpy(),

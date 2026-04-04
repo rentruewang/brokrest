89e7ade94753589587ee407d2cd60b5246237ac3
RenChu Wang <patrick1031wang@gmail.com>
2026-04-04 16:50:03 +0800

    fmt

--- src/brokrest/data/yquery.py
+++ src/brokrest/data/yquery.py
@@ -17,35 +17,11 @@ _WEEK = "wk"
 _YEAR = "y"
 
 Interval = typing.Literal[
-    "1m",
-    "2m",
-    "5m",
-    "15m",
-    "30m",
-    "60m",
-    "90m",
-    "1h",
-    "1d",
-    "5d",
-    "1wk",
-    "1mo",
-    "3mo",
+    "1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"
 ]
 
 Period = typing.Literal[
-    "1d",
-    "5d",
-    "7d",
-    "60d",
-    "1mo",
-    "3mo",
-    "6mo",
-    "1y",
-    "2y",
-    "5y",
-    "10y",
-    "ytd",
-    "max",
+    "1d", "5d", "7d", "60d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"
 ]
 
 

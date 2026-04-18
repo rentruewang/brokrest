3c251fdb7a893a70750f2878da782a2fd5563663
RenChu Wang <patrick1031wang@gmail.com>
2026-04-18 08:11:21 +0800

    more assert

--- src/brokrest/topos/candles.py
+++ src/brokrest/topos/candles.py
@@ -152,6 +152,7 @@ class Candle(Shape, abc.ABC):
             top = self.high
             bottom = self.low
 
+        assert (top >= bottom).all()
         top_coords = torch.stack([top, self.center]).T
         bottom_coords = torch.stack([bottom, self.center]).T
 
